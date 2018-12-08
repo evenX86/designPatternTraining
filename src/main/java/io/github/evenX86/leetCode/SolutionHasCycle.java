8044ba5abde74adb1733dff205934abc78c66a0d
xuyifei <evenX86@gmail.com>
2018-12-08 22:38:34 +0800

    detectCycle

--- src/main/java/io/github/evenX86/leetCode/SolutionHasCycle.java
+++ src/main/java/io/github/evenX86/leetCode/SolutionHasCycle.java
@@ -13,18 +13,45 @@ public class SolutionHasCycle {
         }
         return false;
     }
+
+    /**
+     * 在第一版的基础上找到meet node 后找到start node
+     * 代码简单，但蕴含代码背后的数学证明比较复杂，推荐视频讲解：https://www.youtube.com/watch?v=LUm2ABqAs1w
+     * 可以证明head to start node 要走的路径和meet node 到start node 要走的路径 * loop 的整数倍是相同。
+     * 所以reset to head之后开始走和meet node开始走，肯定可以共同走到start node
+     * @param head
+     * @return
+     */
+    public static ListNode detectCycle(ListNode head) {
+        if (head == null) return head;
+        ListNode fast=head,slow=head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) break;
+        }
+        if (fast.next == null || fast.next.next == null) return null; // 无环处理
+        while (head != slow) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return head;
+    }
     public static void main(String[] args) {
         ListNode listNode = new ListNode(1);
-        ListNode listNode1 = new ListNode(2);
-
-        listNode.next = listNode1;
-        listNode1.next = new ListNode(3);
-        listNode.next.next = listNode1;
+        ListNode listNode1 = new ListNode(5);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+        listNode.next.next.next.next = listNode1;
+        listNode.next.next.next.next.next = new ListNode(6);
+        listNode.next.next.next.next.next.next = new ListNode(7);
+        listNode.next.next.next.next.next.next.next = listNode1;
 
 
         /*listNode.next.next.next = new ListNode(4);
         listNode.next.next.next.next = new ListNode(5);
         listNode.next.next.next.next.next = new ListNode(6);*/
-        System.out.println(hasCycle(listNode));
+        System.out.println(detectCycle(listNode).val);
     }
 }
