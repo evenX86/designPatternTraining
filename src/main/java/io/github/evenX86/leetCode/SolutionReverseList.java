0538041679bfa2130a7ad9cbc49d4ba2847fb70a
xuyifei <evenX86@gmail.com>
2018-12-09 14:32:13 +0800

    reverseKGroup k个子链表反转

--- src/main/java/io/github/evenX86/leetCode/SolutionReverseList.java
+++ src/main/java/io/github/evenX86/leetCode/SolutionReverseList.java
@@ -20,14 +20,58 @@ public class SolutionReverseList {
         return res;
     }
 
+    /**
+     * 链表翻转相邻k个元素
+     * @param head
+     * @param k
+     * @return
+     */
+    public static ListNode reverseKGroup(ListNode head, int k) {
+        ListNode from = head, to=head,preTo=head;
+        int count = 0, i;
+        if(head == null || k < 2) return head;
+        ListNode iter = head;
+        for (;;) {
+            ListNode res = null;
+            ListNode tmp1 = iter;
+            for (i =1;i<=k && tmp1 != null;i++) {
+                tmp1 = tmp1.next; // 保存数量不足的group
+            }
+            if (i == k+1) {
+                for (i=1;i<=k;i++) {
+                    ListNode tmp = new ListNode(iter.val);
+                    if (i==1) to = tmp;
+                    tmp.next = res;
+                    res  = tmp;
+                    iter = iter.next;
+                    if (iter == null) {
+                        break;
+                    }
+                }
+            } else { // 数量不足的反转段肯定到了处理尽头，跳出处理循环
+                if (count > 0) preTo.next = iter; // 如果是第一次就不足k个，跳出，不反转
+                break;
+            }
+            count ++; // 处理次数
+            if (count == 1) {
+                from = res;
+            }
+            if (count > 1) {
+                preTo.next = res;
+            }
+            preTo = to; // 记录上一次的后继指针
+            if (iter == null) break;
+        }
+
+        return from;
+    }
+
     public static void main(String[] args) {
         ListNode listNode = new ListNode(1);
-        listNode.next = new ListNode(2);
-        listNode.next.next = new ListNode(3);
-        ListNode newNode = reverseList(listNode);
+        ListNode newNode = reverseKGroup(listNode,2);
         //ListNode newNode = listNode;
         while (newNode != null) {
-            System.out.println(newNode.val);
+            System.out.print(newNode.val + "->");
             newNode = newNode.next;
         }
 
