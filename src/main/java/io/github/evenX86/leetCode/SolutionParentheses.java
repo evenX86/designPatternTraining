1de5a26a29de8a8e218994bf06ab4b8f7b11159b
xuyifei <evenX86@gmail.com>
2018-12-13 08:24:10 +0800

    优化代码

--- src/main/java/io/github/evenX86/leetCode/SolutionParentheses.java
+++ src/main/java/io/github/evenX86/leetCode/SolutionParentheses.java
@@ -1,35 +1,34 @@
 package io.github.evenX86.leetCode;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class SolutionParentheses {
     public static boolean isValid(String s) {
 
         if (s == null || s.length() == 0) return true;
+        final Map<Character, Character> chMap = new HashMap<Character, Character>() {
+            {
+                put('}','{');
+                put(']','[');
+                put(')','(');
+            }
+        };
         Stack<Character> stack = new Stack<Character>();
         for (int i =0 ;i <s.length(); i ++) {
             char ch = s.charAt(i);
-            if (stack.empty()) {
+            if (!chMap.containsKey(ch)) {
                 stack.push(ch);
-                continue;
-            }
-            Character character = stack.peek();
-            if (
-                    (ch == '}' && character == '{') ||
-                    (ch == ']' && character == '[') ||
-                    (ch == ')' && character == '(')
-             ) {
-                stack.pop();
-            } else {
-                stack.push(ch);
+            } else if (stack.isEmpty() || stack.pop() != chMap.get(ch)){
+                return false;
             }
         }
-        if (stack.empty()) return true;
-        return false;
+        return stack.empty();
 
     }
     public static void main(String[] args) {
-        System.out.println(isValid(""));
+        System.out.println(isValid("]"));
 
     }
 }
