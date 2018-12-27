640b1efa9afea392d0c53bb24e4dca11fd08fa04
xuyifei <evenX86@gmail.com>
2018-12-27 23:49:18 +0800

    Solution isAnagram

---
+++ src/main/java/io/github/evenX86/leetCode/SolutionIsAnagram.java
@@ -0,0 +1,32 @@
+package io.github.evenX86.leetCode;
+
+import java.util.HashMap;
+
+public class SolutionIsAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for (Character a : s.toCharArray()) {
+            if (map.containsKey(a)) {
+                map.put(a, map.get(a) + 1);
+            } else {
+                map.put(a, 1);
+            }
+        }
+        for (Character a : t.toCharArray()) {
+            if (map.containsKey(a)) map.put(a,map.get(a) -1);
+            else return false;
+            if (map.get(a) == 0) map.remove(a);
+        }
+        if (map.isEmpty()) return true;
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(isAnagram("sss","ttt"));
+        int sum = 0;
+        for (Character a : "sss".toCharArray()) {
+            sum += a;
+            System.out.println(sum);
+        }
+    }
+}
