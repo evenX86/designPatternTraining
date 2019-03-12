9261a76e4057116e037aed3f45fae135ebef6032
xuyifei <evenX86@gmail.com>
2019-03-12 23:18:52 +0800

    add note

--- src/main/java/io/github/evenX86/leetCode/SolutionClimbStairs.java
+++ src/main/java/io/github/evenX86/leetCode/SolutionClimbStairs.java
@@ -3,6 +3,24 @@ package io.github.evenX86.leetCode;
 /**
  * Created with design-pattern-training
  *
+ *
+ dp[6] = dp[5] + dp[4]
+
+ dp[n] = dp[n-1] + dp[n-2]
+
+ dp[1] = 1
+ dp[2] = 2
+
+ 递归+记忆化
+
+ dp[i] 是 有可能被重复计算的
+
+ 增加mem[i] 保存中间状态
+ dp[3] = dp[2] + dp[1]
+
+ 状态转移公式：
+
+ dp[i] = dp[i01]
  * @author xuyifei
  * @date 2019-03-12 11:13 PM
  */
