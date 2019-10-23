705eb6e6d8499cdf2d08b579a513b80352b8d672
xuyifei <evenX86@gmail.com>
2019-10-23 21:48:26 +0800

    similar code

--- src/main/java/io/github/evenX86/algo/sort/InsertSort.java
+++ src/main/java/io/github/evenX86/algo/sort/InsertSort.java
@@ -20,8 +20,7 @@ public class InsertSort implements BaseSort {
         //4,5,7,1,6,2
         //1,4,5,7,6,2
         for (int i= 1 ; i< n ; i++) {
-            int val = arr[i];
-            int j = i -1;
+            int val = arr[i],j = i -1;
             for (;j>0; j--) {
                 if (arr[j] > val) {
                     arr[j+1] = arr[j];
